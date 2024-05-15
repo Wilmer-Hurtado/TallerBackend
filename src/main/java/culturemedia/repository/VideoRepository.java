0f791bf2f68f2477ada1755633bd8ef570b7a966
Wilmer Hurtado <wahr2011@gmail.com>
2024-05-15 14:33:45 -0500

    Adding video repository test cases

--- src/main/java/culturemedia/repository/VideoRepository.java
+++ src/main/java/culturemedia/repository/VideoRepository.java
@@ -1,9 +1,11 @@
 package culturemedia.repository;
 import java.util.List;
 import culturemedia.model.Video;
-public interface VideoRepository {
+public interface VideoRepository<click> {
     List<Video> findAll();
     Video save(Video video);
-    List<Video> findByTitle(String title);
-    List<Video> findByDuration(Double fromDuration, Double toDuration);
+
+    List<Video> find(String click);
+
+    click find(double v, double v1);
 }
