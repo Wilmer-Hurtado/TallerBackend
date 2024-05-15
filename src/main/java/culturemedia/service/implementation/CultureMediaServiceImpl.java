09e272553d222d31e9f569349fc8e250c8a621a3
Wilmer Hurtado <wahr2011@gmail.com>
2024-05-15 13:24:05 -0500

    Adding service-implementation

---
+++ src/main/java/culturemedia/service/implementation/CultureMediaServiceImpl.java
@@ -0,0 +1,24 @@
+package culturemedia.service.implementation;
+import culturemedia.model.Video;
+import culturemedia.model.VideoPlayback;
+import culturemedia.repository.VideoRepository;
+import culturemedia.repository.ViewsRepository;
+import culturemedia.service.implementation.CultureMediaService;
+
+import java.util.List;
+public abstract class CultureMediaServiceImpl implements CultureMediaService {
+    private VideoRepository videoRepository;
+    private ViewsRepository viewsRepository;
+    public List<Video> findAll() {
+        return videoRepository.findAll();
+    }
+
+    public Video save(Video video)  {
+        return videoRepository.save(video);
+    }
+
+    public VideoPlayback save(VideoPlayback view)  {
+        return viewsRepository.save(view);
+    }
+
+}
